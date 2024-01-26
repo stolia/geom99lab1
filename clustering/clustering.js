1891663ad119fc98b1f0d30a502073bf27b2e300
stolia <114169045+stolia@users.noreply.github.com>
2024-01-26 12:00:55 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: { lat: 105.380, lng: 62.227 },
+    center: { lat: -28.024, lng: 140.887 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
